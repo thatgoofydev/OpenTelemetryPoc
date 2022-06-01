a0887d4769c75f8d96292b7b898076b364a55327
Jeroen Verwimp <jeroen@verwimp.me>
2022-06-01 20:06:08 +0200

    Add some random delays

--- WeatherApi/Controllers/WeatherForecastController.cs
+++ WeatherApi/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using WeatherApi.Models;
 using WeatherApi.ServiceClients;
@@ -18,12 +19,31 @@ public class WeatherForecastController : ControllerBase
     [HttpGet]
     public async Task<IActionResult> Get([FromQuery] string? city = null)
     {
-        Coordinates? coordinates = null;
+        Coordinates? coordinates = null; 
         if (city != null)
         {
-            coordinates = await _locationService.GetCoordinates(city);
+            coordinates = await _locationService.GetCoordinates(city)!;
         }
         
+        if (Random.Shared.Next(2) == 0)
+        {
+            await SomeAction();
+            await SomeAction();
+            await SomeAction();
+            await SomeAction();
+            await SomeAction();
+        }
+        else
+        {
+            await Task.WhenAll(
+                SomeAction(),
+                SomeAction(),
+                SomeAction(),
+                SomeAction(),
+                SomeAction()
+            );
+        }
+
         return Ok(new
         {
             Coordinates = coordinates,
@@ -31,6 +51,12 @@ public class WeatherForecastController : ControllerBase
         });
     }
 
+    private static async Task SomeAction()
+    {
+        using var activity = Telemetry.Source.StartActivity();
+        await Task.Delay(TimeSpan.FromMilliseconds(Random.Shared.Next(50)));
+    }
+    
     private static readonly string[] Summaries = { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
     private static WeatherForecast[] GetRandomForecast()
     {
